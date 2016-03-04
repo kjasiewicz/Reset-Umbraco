802b903d58d78fff9212e9dfc0d550c1ee16d7d4
Tomasz Kamiński <tkaminski93@gmail.com>
2016-03-04 13:16:41 +0100

    aktualności so op

---
+++ Strona RESET Umbraco/App_Plugins/FontAwesomeIconPicker/fontawesomeiconpicker.controller.js
@@ -0,0 +1,43 @@
+﻿angular.module("umbraco")
+    .controller("Rhythm.FontAwesomeIconPickerController",
+        function($scope) {
+            //empty for now
+        });
+
+angular.module('umbraco')
+    .directive('fontAwesomeIconPicker', function(assetsService) {
+    return {
+        restrict: 'C', //matches class 'font-awesome-icon-picker'
+        link: function(scope, el, attrs) {
+
+                var PLUGIN_PATH = "/App_Plugins/FontAwesomeIconPicker";
+
+                assetsService.load([
+                        PLUGIN_PATH + '/vendor/select2/select2.min.js?c=7',
+                    ])
+                    .then(function() {
+                        var format = function(value) {
+                                return '<i class="fa ' + value.text + '"></i> ' + value.text;
+                            },
+                            options = {
+                                formatSelection: format,
+                                formatResult: format,
+                                escapeMarkup: function(m) { return m; },
+                                width: 'element',
+                                allowClear: true,
+                                placeholder: "None"
+                            };
+
+                        //Ensure the custom select box loads after the first Angular digest
+                        //so that the initial value matches model.value
+                        setTimeout(function() {
+                            $(el).select2(options);
+                        }, 0);
+                });
+
+                assetsService.loadCss(PLUGIN_PATH + '/vendor/select2/select2.css?c=7');
+                assetsService.loadCss(PLUGIN_PATH + '/vendor/fontawesome/css/font-awesome.min.css?c=1');
+                assetsService.loadCss(PLUGIN_PATH + '/fontawesomeiconpicker.css?c=7');
+        }
+    };
+});
---
+++ Strona RESET Umbraco/FrontendHelpers/NewsCssClassResolver.cs
@@ -0,0 +1,30 @@
+﻿namespace Strona_RESET_Umbraco.FrontendHelpers
+{
+    public static class NewsCssClassResolver
+    {
+        public static string GetMixItUpCategoryCssClass(string newsType)
+        {
+            switch (newsType)
+            {
+                case "Wykład":
+                {
+                    return "category-lectures";
+                }
+                case "Konferencja":
+                    {
+                        return "category-conference";
+                    }
+                case "Sukces":
+                    {
+                        return "category-success";
+                    }
+
+                case "Inne":
+                default:
+                {
+                    return "category-other";
+                }
+            }
+        }
+    }
+}
