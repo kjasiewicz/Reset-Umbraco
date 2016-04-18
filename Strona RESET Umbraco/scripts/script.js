fec53ada04648b38988a034a52fdcc570f0c7fd5
Tomasz Kamiński <tkaminski93@gmail.com>
2016-04-18 11:13:58 +0200

    np

--- Strona RESET Umbraco/scripts/script.js
+++ Strona RESET Umbraco/scripts/script.js
@@ -23,27 +23,10 @@ $(document).ready(function ($) {
 	});
 	
     
-    /*---------------------------------------------------*/
-    /* Progress Bar
-    /*---------------------------------------------------*/    
-    $('.skill-shortcode').appear(function() {
-  		$('.progress').each(function(){ 
-    		$('.progress-bar').css('width',  function(){ 
-    			return ($(this).attr('data-percentage')+'%')});
-  		});
-	},{accY: -100});	
+
 	
 	
-    /*--------------------------------------------------*/
-    /* Counter
-    /*--------------------------------------------------*/   
-        
-    $('.timer').countTo();
-    $('.counter-item').appear(function() {
-        $('.timer').countTo();
-    },{
-    	accY: -100
-    });    
+  
     
 	
 	/*----------------------------------------------------*/
@@ -54,13 +37,7 @@ $(document).ready(function ($) {
 	$(".nav > li.drop > ul").addClass("dropdown");
 	$(".nav > li.drop > ul.dropdown ul").addClass("sup-dropdown");
 	
-	$('.show-search').click(function() {
-		$('.search-form').fadeIn(300);
-		$('.search-form input').focus();
-	});
-	$('.search-form input').blur(function() {
-		$('.search-form').fadeOut(300);
-	});
+	
 				
 	/*----------------------------------------------------*/
 	/*	Back Top Link
@@ -77,193 +54,11 @@ $(document).ready(function ($) {
     });
     $('.back-to-top').click(function(event) {
         event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 600);
+        $('html, body').animate({ scrollTop: 0 }, 600);
         return false;
-    })
-		
-	/*----------------------------------------------------*/
-	/*	Sliders & Carousel
-	/*----------------------------------------------------*/
-	
-	////------- Touch Slider
-	var time = 4.4,
-		$progressBar,
-		$bar,
-		$elem,
-		isPause,
-		tick,
-		percentTime;
-	$('.touch-slider').each(function(){
-		var owl = jQuery(this),
-			sliderNav = $(this).attr('data-slider-navigation'),
-			sliderPag = $(this).attr('data-slider-pagination'),
-			sliderProgressBar = $(this).attr('data-slider-progress-bar');
-			
-		if ( sliderNav == 'false' || sliderNav == '0' ) {
-			var returnSliderNav = false
-		}else {
-			var returnSliderNav = true
-		}
-		
-		if ( sliderPag == 'true' || sliderPag == '1' ) {
-			var returnSliderPag = true
-		}else {
-			var returnSliderPag = false
-		}
-		
-		if ( sliderProgressBar == 'true' || sliderProgressBar == '1' ) {
-			var returnSliderProgressBar = progressBar
-			var returnAutoPlay = false
-		}else {
-			var returnSliderProgressBar = false
-			var returnAutoPlay = true
-		}
-		
-		owl.owlCarousel({
-			navigation : returnSliderNav,
-			pagination: returnSliderPag,
-			slideSpeed : 400,
-			paginationSpeed : 400,
-			lazyLoad : true,
-			singleItem: true,
-			autoHeight : true,
-			autoPlay: returnAutoPlay,
-			stopOnHover: returnAutoPlay,
-			transitionStyle : "fade",
-			afterInit : returnSliderProgressBar,
-			startDragging : pauseOnDragging
-		});
-		
-	});
-
-    function progressBar(elem){
-		$elem = elem;
-		buildProgressBar();
-		start();
-    }
-	
-    function buildProgressBar(){
-		$progressBar = $("<div>",{
-			id:"progressBar"
-		});
-		$bar = $("<div>",{
-			id:"bar"
-		});
-		$progressBar.append($bar).prependTo($elem);
-    }
-	
-    function pauseOnDragging(){
-      isPause = true;
-    }
-	
-	////------- Projects Carousel
-	$(".projects-carousel").owlCarousel({
-		navigation : true,
-		pagination: false,
-		slideSpeed : 400,
-		stopOnHover: true,
-    	autoPlay: 3000,
-    	items : 4,
-    	itemsDesktopSmall : [900,3],
-		itemsTablet: [600,2],
-		itemsMobile : [479, 1]
-	});
-	
-	
-	
-	////------- Testimonials Carousel
-	$(".testimonials-carousel").owlCarousel({
-		navigation : true,
-		pagination: false,
-		slideSpeed : 2500,
-		stopOnHover: true,
-    	autoPlay: 3000,
-    	singleItem:true,
-		autoHeight : true,
-		transitionStyle : "fade"
-	});
-	
-	
-	
-	
-	
-	
-	////------- Custom Carousel
-	$('.custom-carousel').each(function(){
-		var owl = jQuery(this),
-			itemsNum = $(this).attr('data-appeared-items'),
-			sliderNavigation = $(this).attr('data-navigation');
-			
-		if ( sliderNavigation == 'false' || sliderNavigation == '0' ) {
-			var returnSliderNavigation = false
-		}else {
-			var returnSliderNavigation = true
-		}
-		if( itemsNum == 1) {
-			var deskitemsNum = 1;
-			var desksmallitemsNum = 1;
-			var tabletitemsNum = 1;
-		} 
-		else if (itemsNum >= 2 && itemsNum < 4) {
-			var deskitemsNum = itemsNum;
-			var desksmallitemsNum = itemsNum - 1;
-			var tabletitemsNum = itemsNum - 1;
-		} 
-		else if (itemsNum >= 4 && itemsNum < 8) {
-			var deskitemsNum = itemsNum -1;
-			var desksmallitemsNum = itemsNum - 2;
-			var tabletitemsNum = itemsNum - 3;
-		} 
-		else {
-			var deskitemsNum = itemsNum -3;
-			var desksmallitemsNum = itemsNum - 6;
-			var tabletitemsNum = itemsNum - 8;
-		}
-		owl.owlCarousel({
-			slideSpeed : 300,
-			stopOnHover: true,
-			autoPlay: false,
-			navigation : returnSliderNavigation,
-			pagination: false,
-			lazyLoad : true,
-			items : itemsNum,
-			itemsDesktop : [1000,deskitemsNum],
-			itemsDesktopSmall : [900,desksmallitemsNum],
-			itemsTablet: [600,tabletitemsNum],
-			itemsMobile : false,
-			transitionStyle : "goDown",
-		});
-	});
-	
-    
-    
-    ////------- Testimonials Carousel
-	$(".fullwidth-projects-carousel").owlCarousel({
-		navigation : false,
-		pagination: false,
-		slideSpeed : 400,
-		stopOnHover: true,
-    	autoPlay: 3000,
-    	items : 5,
-    	itemsDesktopSmall : [900,3],
-		itemsTablet: [600,2],
-		itemsMobile : [479, 1]
-	});
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Tabs
-	/*----------------------------------------------------*/
-
-    $('#myTab a').click(function(e) {
-        e.preventDefault();
-        $(this).tab('show');
     });
-	
-	
-	
+		
+
 	
 	/*----------------------------------------------------*/
 	/*	Css3 Transition
@@ -280,86 +75,7 @@ $(document).ready(function ($) {
 		}
 	});
 	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Pie Charts
-	/*----------------------------------------------------*/
-	
-	var pieChartClass = 'pieChart',
-        pieChartLoadedClass = 'pie-chart-loaded';
-		
-	function initPieCharts() {
-		var chart = $('.' + pieChartClass);
-		chart.each(function() {
-			$(this).appear(function() {
-				var $this = $(this),
-					chartBarColor = ($this.data('bar-color')) ? $this.data('bar-color') : "#F54F36",
-					chartBarWidth = ($this.data('bar-width')) ? ($this.data('bar-width')) : 150
-				if( !$this.hasClass(pieChartLoadedClass) ) {
-					$this.easyPieChart({
-						animate: 2000,
-						size: chartBarWidth,
-						lineWidth: 2,
-						scaleColor: false,
-						trackColor: "#eee",
-						barColor: chartBarColor,
-					}).addClass(pieChartLoadedClass);
-				}
-			});
-		});
-	}
-	initPieCharts();
-	
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Animation Progress Bars
-	/*----------------------------------------------------*/
-	
-	$("[data-progress-animation]").each(function() {
-		
-		var $this = $(this);
-		
-		$this.appear(function() {
-			
-			var delay = ($this.attr("data-appear-animation-delay") ? $this.attr("data-appear-animation-delay") : 1);
-			
-			if(delay > 1) $this.css("animation-delay", delay + "ms");
-			
-			setTimeout(function() { $this.animate({width: $this.attr("data-progress-animation")}, 800);}, delay);
 
-		}, {accX: 0, accY: -50});
-
-	});
-	
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Milestone Counter
-	/*----------------------------------------------------*/
-	
-	jQuery('.milestone-block').each(function() {
-		jQuery(this).appear(function() {
-			var $endNum = parseInt(jQuery(this).find('.milestone-number').text());
-			jQuery(this).find('.milestone-number').countTo({
-				from: 0,
-				to: $endNum,
-				speed: 4000,
-				refreshInterval: 60,
-			});
-		},{accX: 0, accY: 0});
-	});
-	
-	
-	
-	
-	
 	/*----------------------------------------------------*/
 	/*	Nivo Lightbox
 	/*----------------------------------------------------*/
@@ -369,57 +85,6 @@ $(document).ready(function ($) {
 		keyboardNav: true,
 		errorMessage: 'The requested content cannot be loaded. Please try again later.'
 	});
-	
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Change Slider Nav Icons
-	/*----------------------------------------------------*/
-	
-	$('.touch-slider').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-	$('.touch-slider').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
-	$('.touch-carousel, .testimonials-carousel').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-	$('.touch-carousel, .testimonials-carousel').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
-	$('.read-more').append('<i class="fa fa-angle-right"></i>');
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-	/*	Tooltips & Fit Vids & Parallax & Text Animations
-	/*----------------------------------------------------*/
-	
-	$("body").fitVids();
-	
-	$('.itl-tooltip').tooltip();
-	
-	$('.bg-parallax').each(function() {
-		$(this).parallax("30%", 0.2);
-	});
-	
-	$('.tlt').textillate({
-		loop: true,
-		in: {
-			effect: 'fadeInUp',
-			delayScale: 2,
-			delay: 50,
-			sync: false,
-			shuffle: false,
-			reverse: true,
-		},
-		out: {
-			effect: 'fadeOutUp',
-			delayScale: 2,
-			delay: 50,
-			sync: false,
-			shuffle: false,
-			reverse: true,
-		},
-	});
-	
-	
 	
 	
 	
@@ -483,94 +148,6 @@ $(document).ready(function ($) {
 		
 		
 	})();
-});
-
-
-
-
-/*----------------------------------------------------*/
-/*	Portfolio Isotope
-/*----------------------------------------------------*/
-
-jQuery(window).load(function(){
-	
-	var $container = $('#portfolio');
-	$container.isotope({
-		layoutMode : 'masonry',
-		filter: '*',
-		animationOptions: {
-			duration: 750,
-			easing: 'linear',
-			queue: false,
-		}
-	});
-
-	$('.portfolio-filter ul a').click(function(){
-		var selector = $(this).attr('data-filter');
-		$container.isotope({
-			filter: selector,
-			animationOptions: {
-				duration: 750,
-				easing: 'linear',
-				queue: false,
-			}
-		});
-	  return false;
-	});
-
-	var $optionSets = $('.portfolio-filter ul'),
-	    $optionLinks = $optionSets.find('a');
-	$optionLinks.click(function(){
-		var $this = $(this);
-		if ( $this.hasClass('selected') ) { return false; }
-		var $optionSet = $this.parents('.portfolio-filter ul');
-		$optionSet.find('.selected').removeClass('selected');
-		$this.addClass('selected'); 
-	});
-	
-});
-/* ----------------- End JS Document ----------------- */
-
-$(document).ready(function(){
-	//Top Bar Switcher
-	$(".topbar-style").change(function(){
-		if( $(this).val() == 1){
-			$(".top-bar").removeClass("dark-bar"),
-			$(".top-bar").removeClass("color-bar"),
-			$(window).resize();
-		} else if( $(this).val() == 2){
-			$(".top-bar").removeClass("color-bar"),
-			$(".top-bar").addClass("dark-bar"),
-			$(window).resize();
-		} else if( $(this).val() == 3){
-			$(".top-bar").removeClass("dark-bar"),
-			$(".top-bar").addClass("color-bar"),
-			$(window).resize();
-		}
-	});
-	
-	//Layout Switcher
-	$(".layout-style").change(function(){
-		if( $(this).val() == 1){
-			$("#container").removeClass("boxed-page"),
-			$(window).resize();
-		} else{
-			$("#container").addClass("boxed-page"),
-			$(window).resize();
-		}
-	});
-	
-	//Background Switcher
-	$('.switcher-box .bg-list li a').click(function() {
-		var current = $('.switcher-box select[id=layout-style]').find('option:selected').val();
-		if(current == '2') {
-			var bg = $(this).css("backgroundImage");
-			$("body").css("backgroundImage",bg);
-		} else {
-			alert('Please select boxed layout');
-		}
-	});
-
 });
 
 /**
